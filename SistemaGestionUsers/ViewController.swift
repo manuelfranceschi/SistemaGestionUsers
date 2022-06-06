faeb75719d93822b95b054205e81f70b47349475
Mnauel <manuelfranceschineves@gmail.com>
2022-06-06 14:59:30 +0200

    Initial Commit

---
+++ SistemaGestionUsers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SistemaGestionUsers
+//
+//  Created by Manuel Franceschi on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
